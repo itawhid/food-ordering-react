c6a9a80147a9586ff7802cdd1130fe38951a7fe1
Tawhidul Islam <tawhidul.islam@spaceteams.de>
2022-12-28 20:03:33 +0100

    feat: added cart modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
