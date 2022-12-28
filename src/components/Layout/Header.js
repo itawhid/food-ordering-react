96ad155ef1c6f5a06d627236790e84f3860329fc
Tawhidul Islam <tawhidul.islam@spaceteams.de>
2022-12-28 16:13:21 +0100

    feat: added cart component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import mealsImage from '../../assests/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
